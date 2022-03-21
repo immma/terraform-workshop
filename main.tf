f364069782f2d221e9155f45b414c08b71697584
immma <immadiarabdul@gmail.com>
2022-03-21 12:13:09 +0700

    update variable

--- main.tf
+++ main.tf
@@ -1,5 +1,17 @@
 provider "aws" {
-  region = "ap-southeast-2"
+  region     = "ap-southeast-2"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+variable "access_key" {
+  type        = string
+  description = "AWS access key"
+}
+
+variable "secret_key" {
+  type        = string
+  description = "AWS access key"
 }
 
 # data "aws_ami" "amazon-linux-2" {
