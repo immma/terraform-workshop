3601b66a3fc3141d78c0039e3504b97ef28cb46c
immma <immadiarabdul@gmail.com>
2022-03-21 11:08:10 +0700

    initial commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amazon-linux-2.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "terraform-workshop"
+  }
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "ardih-terraform-bucket"
+}
